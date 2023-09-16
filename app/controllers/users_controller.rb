8a1c921a6b9e5184f63f8a3f0aaf585a792215cf
yamagisitutomu <ec2-user@ip-172-31-0-91.ap-northeast-1.compute.internal>
2023-09-16 10:05:31 +0000

    peagekannsei

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    @book = Book.new
+    @current_user = current_user
+  end
+
+
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
+    @current_user = current_user
   end
 
   def edit
