185fdb3a72944dccabaf206f90cb345801ac37ad
yamagisitutomu <ec2-user@ip-172-31-0-91.ap-northeast-1.compute.internal>
2023-09-22 11:01:49 +0000

    ouyou3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,64 +1,54 @@
 class BooksController < ApplicationController
-  before_action :correct_user, only: [:edit]
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book.id)
-    else
-      @books = Book.all
-      render :index
-    end
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
+  def show
+    @book = Book.find(params[:id])
+    @book_comment = BookComment.new
   end
 
   def index
     @books = Book.all
     @book = Book.new
-    @current_user = current_user
   end
 
-  def show
-    @books = Book.find(params[:id])
-    @user = @books.user
-    @current_user = current_user
-    @book = Book.new
-    @book_comment = BookComment.new
-  end
-
-  def edit
-    @book = Book.find(params[:id])
-  end
-
-  def update
-    @book = Book.find(params[:id])
-    if @book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
-    redirect_to book_path(@book.id)
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
     else
-    render :edit
+      @books = Book.all
+      render 'index'
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
-    redirect_to '/books'
+    redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :body,)
+    params.require(:book).permit(:title, :body)
   end
-  
-  def correct_user
+
+  def ensure_correct_user
     @book = Book.find(params[:id])
-    @user = @book.user
-    redirect_to(books_path) unless @user == current_user
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
-  
-  
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  get "/homes/about" => "homes#about", as: "about"
-  resources :books do
+  get "home/about"=>"homes#about"
+  
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
    resources :book_comments, only: [:create, :destroy]
    resource :favorites, only: [:create, :destroy]
   end
